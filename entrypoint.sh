e3da234807ce1021de338ea0bc8e5ad4c4a95073
Akaronte <akaronte@gmail.com>
2022-03-06 01:43:49 +0100

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if test -f "/home/sfserver/Satisfactory/FactoryServer.sh"; then
+if test -f "/home/sfserver/steamcmd_linux.tar.gz"; then
 sh /home/sfserver/Satisfactory/FactoryServer.sh
 else
 wget -O /home/sfserver/steamcmd_linux.tar.gz https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz && tar xf /home/sfserver/steamcmd_linux.tar.gz && /home/sfserver/steamcmd.sh +force_install_dir /home/sfserver/Satisfactory +login anonymous +app_update 1690800 -beta public validate +quit && \
